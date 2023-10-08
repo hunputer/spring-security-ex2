950721cddc68943279b9850eadf5aac2f1561e5e
hunputer <wlgns3855@gmail.com>
2023-10-08 22:15:37 +0900

    spring-security-ex14

--- src/main/java/com/example/springsequrity2/controller/MainController.java
+++ src/main/java/com/example/springsequrity2/controller/MainController.java
@@ -24,7 +24,7 @@ public class MainController {
     @GetMapping("/")
     public String main(HttpServletRequest request){
 
-        LoginUtil.getUser();
+        Account account = LoginUtil.getUser();
 
         return "main";
     }
